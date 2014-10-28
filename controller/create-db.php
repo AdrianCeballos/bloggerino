fe2f92807f1ef00d4ea57bcf5f153110ce278cab
Adrian Ceballos <zzAdrianCeballos@gmail.com>
2014-10-28 11:00:13 -0700

    comments

--- controller/create-db.php
+++ controller/create-db.php
@@ -2,14 +2,17 @@
     /*looks at certain file and stops scripts if not there*/
     require_once(__DIR__."/../model/database.php");
     $connection = new mysqli($host, $username, $password);
-        /*if else statement that lets you know if connection is working*/
+        /*if else statement that lets you know if connection is working if it isnt it send error*/
         if ($connection->connect_error){
             die("Error" . $connection->connect_error);
         }
-        $exists=$connection->select_db($database);  
+        /*Variable that selects the database*/
+        $exists=$connection->select_db($database);
+           /*checks to see if database exists if it doesnt it creates it*/
             if (!$exists){
                 $query = $connection->query("CREATE DATABASE $database");
             }
+            /*tells you if you create it successfully*/
             if ($query) {
                 echo "Successfully created database". $database;
             }
