e81095c5a78adfd2a2950c041132d63dee1f0efd
Adrian Ceballos <zzAdrianCeballos@gmail.com>
2014-10-31 11:12:15 -0700

    tells if table there

--- controller/create-db.php
+++ controller/create-db.php
@@ -4,7 +4,7 @@
     $connection = new mysqli($host, $username, $password);
         /*if else statement that lets you know if connection is working if it isnt it send error*/
         if ($connection->connect_error){
-            die("Error" . $connection->connect_error);
+            die("<p>Error" . $connection->connect_error."</p>");
         }
         /*Variable that selects the database*/
         $exists = $connection->select_db($database);
@@ -14,20 +14,28 @@
             
             /*tells you if you create it successfully*/
             if ($query) {
-                echo "Successfully created database " . $database;
+                echo "</p>Successfully created database " . $database ."</p>";
             }
             }
             /*if it does exist then it echos*/
             else{
-                echo "Database already exists";
+                echo "<p>Database already exists</p>";
             }
             /*creating a query that makes a table in the database to store info
             then create an id for each blog post the table consists of id title and post not null
              means it cannot be empty*/
             $query = $connection ->query("CREATE TABLE posts ("
-                ."id int(11) NOT NULL AUTO_INCREMENT, "
+                    ."id int(11) NOT NULL AUTO_INCREMENT, "
                     ."title varchar(255) NOT NULL,"
-                    ."post text NOT NULL"
+                    ."post text NOT NULL,"
                     ."PRIMARY KEY (id))");
+            if ($query){
+                echo "<p>Successfully created the table: posts</p>";
+            }
+            else {
+                echo "<p>$connection->error</p>";
+            }
+            
+                
             
         $connection->close();
