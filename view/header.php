4e34290224a051b6ef0127b7c0d87446b6cb705a
Adrian Ceballos <zzAdrianCeballos@gmail.com>
2014-11-06 11:05:35 -0800

    Added media queries

--- view/header.php
+++ view/header.php
@@ -2,7 +2,7 @@
     
     <head>
         <meta charset="UTF-8">
-        <link rel="stylesheet" type="text/css" href="main.css">
+        <link rel="stylesheet" type="text/css" href="css/main.css">
         <title></title>
     </head>
     <body class="pls">
