e743b93a2cbe4da7de3adcfb3ec31c75ec0d3a79
Adrian Ceballos <zzAdrianCeballos@gmail.com>
2014-12-16 20:15:49 -0800

    crypted password

--- controller/create-post.php
+++ controller/create-post.php
@@ -10,7 +10,7 @@
     $query = $_SESSION["connection"] ->query("INSERT INTO posts SET title ='$title', post = '$post'");
     if ($query){
         echo "<p>Successfully instered:$title</p>";
-        echo "Posted on:". $date->format("M/D/Y")." at ". $time->format("g:i");
+        echo "Posted on:". $date->format("m/d/y")." at ". $time->format("G:i");
         
     }
     else{
