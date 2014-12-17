f9b62ffc6918fc2ae0c020cbb0a08b79e1e41f00
Adrian Ceballos <zzAdrianCeballos@gmail.com>
2014-12-16 17:39:30 -0800

    date and time

--- controller/create-post.php
+++ controller/create-post.php
@@ -5,9 +5,13 @@
     
     $title = filter_input(INPUT_POST,"title",FILTER_SANITIZE_STRING);
     $post = filter_input(INPUT_POST,"post",FILTER_SANITIZE_STRING);
+    $date = new DateTime('today');
+    $time = new DateTime('America/Los_Angeles');
     $query = $_SESSION["connection"] ->query("INSERT INTO posts SET title ='$title', post = '$post'");
     if ($query){
         echo "<p>Successfully instered:$title</p>";
+        echo "Posted on:". $date->format("M/D/Y")." at ". $time->format("g:i");
+        
     }
     else{
         echo "<p>" . $_SESSION["connection"] ->error . "</p>";
